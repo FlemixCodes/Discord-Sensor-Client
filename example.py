dbfe1a7051b216bb26d5c58f763392b228577d31
Pavel <pavelvodocnikov@gmail.com>
2025-08-17 06:40:55 +0500

    обновил пример

--- example.py
+++ example.py
@@ -5,18 +5,28 @@ from discord_sensor import DiscordSensorClient
 
 
 async def main():
+    # Инициаилизируем объект клиента
     client = DiscordSensorClient()
 
+    # Получаем все сервера по рейтингу
     servers = await client.servers.get_servers_by_rating()
+
+    # Получаем айди первого сервера
     server_id = servers["guilds"][0]["id"]
 
+    # Получаем информацию о сервере
     server_info = await client.servers.get_server_info(server_id)
-    print(server_info)
+    pprint(server_info)
 
+    # Получаем роли сервера
     server_roles = await client.functions.get_server_roles(server_id)
+
+    # Берем первую роль
     role_id = server_roles["roles"][0]["role_id"]
+
+    # Получаем всех участников с этой ролью
     role_members = await client.functions.get_role_members(server_id, role_id, 1, 1)
-    print(role_members)
+    pprint(role_members)
 
 
 if __name__ == "__main__":
